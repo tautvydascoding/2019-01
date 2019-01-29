7db7e967827afb05a8adf3d797dcbe74069a70d8
Deividas <deivissms@gmail.com>
2019-01-29 11:01:46 +0200

    tuscias

---
+++ dirbame_cia/diena_6/destytojas/1-js-pagrindai/main.js
@@ -0,0 +1,39 @@
+console.log(   " mano js failas "  );
+
+let sk = 2019;
+console.log("skiacius yra", sk);
+sk = 2020;   //  pakeitem kintamaji
+console.log("skaicius po pakeitimo", sk);
+sk = sk + 1;    // sk = 2020 + 1;
+console.log("skaicius po pakeitimo", sk);
+sk = sk + sk;    // sk = 2020 + 2020;
+console.log("skaicius po pakeitimo", sk);
+
+
+// sukurti 3 kintamuosius: x, y ,z;
+// x ir y priskirti reiksmes (inicilizuoti)
+// uzduotis - apskaiciuoti suma
+let x;
+let y;
+let z;
+x = 5;
+y = 10;
+z = x + y;
+console.log( "x  + y = ", z);
+console.log( " suma: ", z);
+console.log( "x:", x , "y:", y, " x  + y = ", z);
+
+// matematika
+z = x * y;
+console.log("daugyba", z);
+z  = ((2 + 4) / (x * 3))  - 1;
+
+// z = ((((x + 1))));
+
+// Math - javascript matematikos biblioteka
+z = Math.sqrt(16);
+console.log("16 saknis:", z);
+
+
+
+//
