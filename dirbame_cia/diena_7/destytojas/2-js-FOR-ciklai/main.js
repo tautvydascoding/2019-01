5a4c2168def56bf1b54e72b183301f652dfdcd36
Deividas <deivissms@gmail.com>
2019-01-30 17:19:18 +0200

    day 7 - works

---
+++ dirbame_cia/diena_7/destytojas/2-js-FOR-ciklai/main.js
@@ -0,0 +1,33 @@
+console.log(   " mano js failas "  );
+
+
+
+for (var i = 0; i < 9  ; i++) {
+     console.log(" labas", i );
+}
+
+let x = 10;
+x = x + 1;   // 11
+x = x + 1;   // 12
+//
+x++;   // x = x + 1;
+x--;   // x = x - 1;
+x+4;   // NEVEIKIA
+//
+ x += 1;    // x = x + 1;
+ x += 4;    // x = x + 4;
+
+ x *= 4;    // x = x * 4;
+ x -= 4;    // x = x - 4;
+
+let s = "istorija prasidejo";
+s += " autorius K.G.";    // s = s + " autorius K.G.";
+console.log( s );
+
+
+let tekstas = "";
+for (var i = 0; i < 8; i++) {
+    tekstas = tekstas + "@";
+    console.log(  tekstas );
+}
+//
