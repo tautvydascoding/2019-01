d9072a4f885b619efa51ecf61af384183b830d0d
Deividas <deivissms@gmail.com>
2019-02-04 17:02:12 +0200

    day 10 works

--- dirbame_cia/diena_10/destytojas/1-js-scopes/main.js
+++ dirbame_cia/diena_10/destytojas/1-js-scopes/main.js
@@ -1,35 +1,35 @@
+console.log(   " mano js failas "  );
 // ====================Scope====================
 //   UZDUOTIS  A
 // sukurti funkcija "printDuomenys()" ,
 // funkcijoje sukurti kintamaji: var vardas = "Tomas"
 // kuri  atspausdina i konsole  kintamaji "vardas"
 // iskviesti f-ja
-
+function printDuomenys() {
+    let vardas = "Tomas";
+    console.log("  let vardas", vardas);
+}
+printDuomenys();
 
 //   UZDUOTIS  B
 //  dabar, pabandykite po f-ja (uz jos ribu) i konsole atspausdinti kintamaji "vardas"
 //  console.log( vardas) ;
-// NOTE: gaunate kalida ar ne?
+// NOTE: gaunate klaida ar ne?
 
+// console.log("  uz ribu vardas", vardas);
 
 //================local && global=========
-// var x = "x -viesas ";
-// function testLocalVar() {
-//    var y = "y -vietinis ";
-//    z = "z -viesas";
-//    console.log("x", x);
-//    console.log("y", y);
-//    console.log("z", z);
-// }
- // KLAUSIMAS:  pries paleidziant/atkomentuojant    consoles (apacioje)- atspekite kurios nesuveiks
-// console.log("x", x);
-// console.log("z", z);
-// console.log("y", y);
- // KLAUSIMAS:  pries paleidziant f-ja - atspekite kurios  consoles (f-joje) nesuveiks
-// testLocalVar();
- // KLAUSIMAS:  pries paleidziant/atkomentuojant    consoles (apacioje)- atspekite kurios nesuveiks
-// console.log("x", x);
-// console.log("z", z);
+var x = "x -viesas ";
+function testLocalVar() {
+   var y = "y -vietinis ";
+   z = "z -viesas";
+   console.log("x", x);  // x viesas
+   console.log("y", y);  //  vietinis
+   console.log("z", z);  // viesas
+}
+testLocalVar();
+console.log("x", x);
+console.log("z", z);
 // console.log("y", y);
 
 //------
@@ -40,33 +40,28 @@
  // D) atspausdinti abu f-jos viduje
  // E) atspausdinti  uz f-jos
 
- // var katinas = "juodas";
+ var katinas = "juodas";
+
+ function test2() {
+     var katinas = "baltas";
+    console.log("1 katinas:", katinas);
+    console.log("2 this.katinas - f-jos viduje: ",  this.katinas);
+ }
+ test2();
  //
- // function test2() {
- //     var katinas = "baltas";
- //    console.log("1 katinas:", katinas);
- //    console.log("2 this.katinas - f-jos viduje: ",  this.katinas);
- // }
- // KLAUSIMAS: Pries paleidziant f-ja - atspekite kurios  consoles (f-joje) nesuveiks
- // test2();
- // KLAUSIMAS: Ka isves konsole "3 katinas" - juodas AR bartas????
- // console.log("3 katinas:", katinas);
+ console.log("3 katinas:", katinas);
 
 //==========================
 
- // var xx = 10;
- //
- // function isvesti( xx ) {
- //     xx = 20;
- //     console.log( "1. xx:", xx);
- //     console.log( "2. this.xx:",   this.xx);
- // }
- // KLAUSIMAS: Ka isves konsole "3. XX" - 10 AR 20 AR klaida????
- // console.log( "3. xx:", xx);
- // KLAUSIMAS: Pries paleidziant f-ja - atspekite Ka isves konsole "1. XX ir 2. xx" - 10 AR 20 AR 999????
- // isvesti( 999 );
- // KLAUSIMAS: Ka isves konsole "4. XX" - 10 AR 20 AR klaida????
- // console.log( "4. xx:", xx);
+ var xx = 10;
+
+ function isvesti( xx ) {
+     xx = 20;
+     console.log( "1. xx:", xx);
+     console.log( "2. this.xx:",   this.xx);
+ }
+ console.log( "3. xx:", xx);
+
 
  //======================
  // var vardas = "Jonas";
@@ -82,6 +77,13 @@
 //  sukurti funkcija "printAntraste(x)" ,
 //  kuri atspausdina i DOM-a (ekrana) "x" reiksme
 //  iskviesti f-ja 3 kartus, kad atspausdintu 3 skirtingas antrastes:  "BMW pinga, nes daugeja", "Greik pageres orai", "Vasaros vis salteja"
+function printAntraste(x) {
+    let tekstas = "<h2>" + x + "</h2>";
+    document.querySelector('body').innerHTML += tekstas;
+}
+printAntraste("Pirma mano antraste");
+printAntraste("Antroji mano antraste");
+printAntraste("Trecioji mano antraste");
 
 //  1.2 UZDUOTIS
 //  sukurti funkcija "printStraipsnis(x)"
