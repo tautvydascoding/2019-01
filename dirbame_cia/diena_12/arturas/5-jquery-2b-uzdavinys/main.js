122c6a48abaf763885eda9e702531a9cc7f1035f
mayoctt@gmail.com <46900928+asilingas@users.noreply.github.com>
2019-02-06 21:50:58 +0200

    12 diena papildomai namie

---
+++ dirbame_cia/diena_12/arturas/5-jquery-2b-uzdavinys/main.js
@@ -0,0 +1,6 @@
+/*jshint esversion: 6 */
+console.log(   " mano js failas "  );
+// B pasisiimti bootstrap suprogramuota 1-5 layouta
+
+// naudojant jquery or js
+// atlikti A 1, 2 uzduoti bet naudojant klases sukeitimo f-ja
