9141cecfef62e6c4e797f12535d16777067671b2
asilingas <46900928+asilingas@users.noreply.github.com>
2019-02-07 16:47:39 +0200

    13 diena

---
+++ dirbame_cia/diena_13/arturas/3-php-include-2/header.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- search Bot'ai pvz: google neskanuos musu svetaines -->
+         <meta name="robots" content="nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>3-php-include-2</title>
+        <link rel="stylesheet" href="libs/bootstrap-4/css/bootstrap.min.css">
+        <!-- !! mano CSS failas visada pats zemiausias -->
+        <link rel="stylesheet" href="css/master.css">
+
+    </head>
+    <body>
