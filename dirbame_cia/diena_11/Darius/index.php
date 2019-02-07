db31e717ec8330c8e3ff5c0c8dc63a69a828b363
dariusfotor <46917912+dariusfotor@users.noreply.github.com>
2019-02-07 09:29:26 +0200

    13 diena

--- dirbame_cia/diena_11/Darius/index.php
+++ dirbame_cia/diena_11/Darius/index.php
@@ -9,16 +9,48 @@
     
 </head>
 <body>
-    
+    <h1>jlkjlk</h1>
 <?php
 //komentaras
 /*ilgas komentas*/
 
-$a = aaa
-$a = aaa
-$a = aaa
-$a = aaa
+$a = "labas";
+echo "bubu";
+echo $a;
+echo "tekstas" . " " . " " .  "<br>"; // sujungti teksta
 
+$slaptazodis = "drakonas1";
+echo $slaptazodis;
+
+// die("paskutinta matyta zinute"); // nutruksta kodo veikimas
+print(" panaudojus print() isvedimas");
+
+$name = "Tomas";
+echo "gyveno karta $name ir trejo broli <br />";
+echo 'gyveno karta $name ir trejo broli <br />';
+
+//primityvus kintamieji
+
+$name = "tomas" //string tipo
+$age = 23;  //numer - integer (sveikas)
+$atlygnimas = 960.50;   //number - float/double (su kableliu)
+$arduotipaskola = false; //boolean
+$mirtiesdata = NULL; // NULL
+//nera zodzio undefined
+
+
+
+//neprimityvus kintamieji
+$miestas = ["Kaunas", "Vilnius"] // masyvas
+// $zmogus = new KlasesPavadinimas(); // objektai
+// $failas; //resurso tipo
+
+$preke1 = ["Macbook", "1.jpg", 1000, "labai geras"];
+$preke2 = ["IBM", "2.jpg", 1400, "prasmatnus"];
+$preke1 = ["Macbook pro", "3.jpg", 1700, "ziauriai geras"];
+$visosprekes
+print_r($preke1); //spausdina masyvus, objektus ir textus
+var_dump($prekes2); //spausdina masyvus, obj, txt ir duomenu tipa
 
 
 
