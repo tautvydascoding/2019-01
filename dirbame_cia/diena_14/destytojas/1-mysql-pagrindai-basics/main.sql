52163e35ac7e7551fc357af710cf1ba3c331c6f4
Deividas <deivissms@gmail.com>
2019-02-08 16:47:58 +0200

    14

--- dirbame_cia/diena_14/destytojas/1-mysql-pagrindai-basics/main.sql
+++ dirbame_cia/diena_14/destytojas/1-mysql-pagrindai-basics/main.sql
@@ -10,3 +10,86 @@
 
 
 -- DB nemokamos - pirkdami HOST paslauga gaunate kiek tik norite DB (nes jos mazai vietos uzema)
+CREATE USER 'tautvydas'@'localhost' IDENTIFIED BY 'tratata' ;
+ALTER USER 'tautvydas'@'localhost' PASSWORD EXPIRE NEVER;
+GRANT ALL PRIVILEGES ON * . * TO 'tautvydas'@'localhost' WITH GRANT OPTION;
+
+
+CREATE DATABASE hospital2;
+-- DB pasirinkimas
+use hospital2;
+-- pasitikrinimui
+show databases;
+-- /
+
+
+
+-- AUTO_INCREMENT   id++;
+-- PRIMARY KEY - unikali reiksme -negalesime kito kokio pat sukurti
+CREATE TABLE doctors (
+   id   INT AUTO_INCREMENT PRIMARY KEY,
+   name varchar(30),
+   lname varchar(30)
+   );
+-- test
+show tables;
+
+
+INSERT INTO doctors VALUES ('', 'Tim', 'Logan');
+-- test
+SELECT * FROM doctors;
+INSERT INTO doctors VALUES (NULL, 'Paul', 'Leo');
+INSERT INTO doctors VALUES (NULL, 'Ona', 'Onute');
+INSERT INTO doctors VALUES (NULL, 'Lita', 'Ora');
+INSERT INTO doctors VALUES (NULL, 'Koris', 'Pita');
+SELECT * FROM doctors;
+
+
+CREATE TABLE patients (
+   id   INT AUTO_INCREMENT PRIMARY KEY,
+   name varchar(30),
+   lname varchar(30),
+   doctor_id INT
+   );
+-- test
+show tables;
+
+INSERT INTO patients VALUES( NULL, "Mark", "John", 2);
+-- test
+SELECT * FROM patients;
+INSERT INTO patients VALUES( NULL, "Tom", "Karlors", 2);
+INSERT INTO patients VALUES( NULL, "Aris", "Tido", 2);
+INSERT INTO patients VALUES( NULL, "Tom", "Karlos", 1);
+INSERT INTO patients VALUES( NULL, "Co", "John", 3);
+INSERT INTO patients VALUES( NULL, "Tom", "Naro", 1);
+INSERT INTO patients VALUES( NULL, "Jili", "Jato", 5);
+SELECT * FROM patients;
+
+-- istrinimas eilutes
+DELETE FROM images
+       WHERE id = 5;
+UPDATE doctors
+       SET lname = "litaitė"
+       WHERE id = 5 AND name = "Lita"
+       LIMIT 1;
+
+UPDATE patients
+       SET lname = "Karlito"
+       WHERE id = 4 AND lname = "Karlos"
+       LIMIT 1;
+
+-- uzduotis:
+-- 0. istrinti paskutine nuotrauka
+-- 0. Pakeisti Litos pavarde i Litaitė
+-- 1. ISVESTI SURIKIUOTUS GYTYTOJUS PAGAL pavarde
+-- 2. ISVESTI GYTYTOJUS kuriu id mazesnis uz 3
+-- 3. ISVESTI GYTYTOJUS kuriu pavarde is raides "L"
+
+SELECT * from doctors
+         ORDER BY lname ASC;
+-- 2
+SELECT * FROM doctors
+         WHERE id < 3;
+-- 3
+SELECT * FROM doctors
+         WHERE lname Like "L%";
--- uzduotys/diena_13/UZDUOTYS/0-uzduotis-sql-select.sql
+++ uzduotys/diena_13/UZDUOTYS/0-uzduotis-sql-select.sql
@@ -1,3 +1,10 @@
+-- uzduotis:
+-- 0. istrinti paskutine nuotrauka
+-- 0. Pakeisti Litos pavarde i Litaitė
+-- 1. ISVESTI SURIKIUOTUS GYTYTOJUS PAGAL pavarde
+-- 2. ISVESTI GYTYTOJUS kuriu id mazesnis uz 3
+-- 3. ISVESTI paciantus kuriu vardas is raides "A"
+
 -- UZDUOTIS  1.1  (SELECT) : ISVESTI 4 IR 5 gydytoja
 -- UZDUOTIS  1.2  (SELECT) : ISVESTI visus gydytojus, kuriu vardas: 'Tim' ARBA pavarde 'Leo'
 -- UZDUOTIS  1.3  (SELECT): isvesti visus gydytojus, kuriu 'id' mazesnis uz 4
