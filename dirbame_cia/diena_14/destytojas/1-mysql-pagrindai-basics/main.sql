5b9229aaaafbbe410068447b2064b804fee87b6c
Deividas <deivissms@gmail.com>
2019-02-08 12:29:37 +0200

    klases darbai 13

---
+++ dirbame_cia/diena_14/destytojas/1-mysql-pagrindai-basics/main.sql
@@ -0,0 +1,12 @@
+-- MYSQL serveris: jame saugomos ir veikai DB7
+-- MYSQL gali tureti tureti daugybe vartotoju/ACC/ users
+-- vartotojai/ACC turi skirtingas teises/privilegijas
+-- pagal nutylejima galingiausias vardu "root"
+
+-- DB valdomos - SQL kalba
+-- DB - skirta duomenims laikyti (pvz excel)
+-- viena DB gali tureti daug lenteliu (pvz kaip exel sheets)
+
+
+
+-- DB nemokamos - pirkdami HOST paslauga gaunate kiek tik norite DB (nes jos mazai vietos uzema)
