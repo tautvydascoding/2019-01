0af8d61cf1063057d6d87ce326174914958ed590
asilingas <46900928+asilingas@users.noreply.github.com>
2019-02-09 13:22:29 +0200

    savaitgalio nd

--- dirbame_cia/diena_14/arturas/2-php-prisijungimas-prieDB-duomenu-paemimas/db-functions.php
+++ dirbame_cia/diena_14/arturas/2-php-prisijungimas-prieDB-duomenu-paemimas/db-functions.php
@@ -17,9 +17,32 @@ function getPrisijungimas () {
 }
 function getDoctor($nr) {
   $manoSQL = "SELECT * FROM doctors WHERE id = $nr";
-  $rezultatai = mysqli_query(getPrisijungimas (),$manoSQL);
+  $rezultatai = mysqli_query(getPrisijungimas(),$manoSQL);
   $rezultatai_masyvas = mysqli_fetch_assoc($rezultatai);
   return $rezultatai_masyvas;
 }
 $pirmasGydytojas = getDoctor(3);
 print_r($pirmasGydytojas);
+
+function getPatients($nr) {
+  $manoSQL = "SELECT * FROM patients WHERE id = $nr";
+  $results = mysqli_query(getPrisijungimas(), $manoSQL);
+  $results_array = mysqli_fetch_assoc($results);
+  return $results_array;
+}
+$antrasPacientas = getPatients(2);
+echo "<br>id: ".$antrasPacientas[id];
+echo "<br>name: ".$antrasPacientas[name];
+echo "<br>lname: ".$antrasPacientas[lname];
+echo "<br>doctors_id: ".$antrasPacientas[doctors_id];
+
+function getImage($nr) {
+  $manoSQL = "SELECT * FROM images WHERE id = $nr";
+  $results = mysqli_query(getPrisijungimas(), $manoSQL);
+  $results_array = mysqli_fetch_assoc($results);
+  return $results_array;
+}
+$treciaNuotrauka = getImage(3);
+echo "<br>id: ".$treciaNuotrauka[id];
+echo "<br>name: ".$treciaNuotrauka[name];
+echo "<br>patients_id: ".$treciaNuotrauka[patients_id];
