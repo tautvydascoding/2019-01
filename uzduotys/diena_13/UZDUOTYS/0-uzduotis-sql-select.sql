8e8744d8335605e1af76ba921a2231f03b007b5d
dariusfotor <46917912+dariusfotor@users.noreply.github.com>
2019-02-11 10:17:03 +0200

    php

--- uzduotys/diena_13/UZDUOTYS/0-uzduotis-sql-select.sql
+++ uzduotys/diena_13/UZDUOTYS/0-uzduotis-sql-select.sql
@@ -10,3 +10,5 @@
 -- UZDUOTIS UPDATE- pakeisti Koris pavarde i Makalas (suradus pagal id)
 -- UZDUOTIS UPDATE- pakeisti Koris pavarde i Paulaitis (suradus pagal id ir pavarde)
 -- UZDUOTIS (select LIKE)- paimti visus gydytojus, kuriu pavarde parsideda is "L" raides
+
+1.1 
