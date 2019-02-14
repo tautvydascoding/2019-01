9ad4d9963ee0e28de99f83548487bb714a71256a
irmantoj <46917943+irmantoj@users.noreply.github.com>
2019-02-14 11:23:26 -0800

    Atnaujintas projektas

---
+++ dirbame_cia/PROJEKTAI/irmantas/AntraVersija/Projektas/galery.php
@@ -0,0 +1,42 @@
+<?php
+require_once("head.php");
+require_once("header.php");
+?>
+<div class="container">
+
+
+<div class="row ">
+  <div class="col aukstis-kontaktai">
+    <!-- <h1 class="mt-2">Darbai</h1> -->
+<div id="bs4-slide-carousel" class="carousel slide" data-ride="carousel" >
+<ol class="carousel-indicators">
+<li data-target="#bs4-slide-carousel" data-slide-to="0" class="active"></li>
+<li data-target="#bs4-slide-carousel" data-slide-to="1"></li>
+<li data-target="#bs4-slide-carousel" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner">
+<div class="carousel-item active">
+<iframe width="1120" height="630" src="https://www.youtube.com/embed/9Y3zD4OsEGA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<div class="carousel-item">
+<iframe width="1120" height="630" src="https://www.youtube.com/embed/9Y3zD4OsEGA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<div class="carousel-item">
+<iframe width="1120" height="630" src="https://www.youtube.com/embed/9Y3zD4OsEGA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+<a class="carousel-control-prev" href="#bs4-slide-carousel" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#bs4-slide-carousel" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
+</div>
+  </div>
+</div>
+</div>
+
+
+<?php require_once("footer-c.php"); ?>
