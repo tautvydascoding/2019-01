f01f81e3be7b47d45650665eae8f4f675b239c9c
vitasidorovaite@gmail.com <46902014+VitaSid@users.noreply.github.com>
2019-02-15 16:54:34 +0200

    18,19 dienos ir PROJEKTAS

---
+++ dirbame_cia/diena_19/vita/3-email-siuntimas/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- search Bot'ai pvz: google neskanuos musu svetaines -->
+         <meta name="robots" content="nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="libs/bootstrap-4/css/bootstrap.min.css">
+        <!-- !! mano CSS failas visada pats zemiausias -->
+        <link rel="stylesheet" href="css/master.css">
+
+    </head>
+    <body>
+
+        <h1> Isijunk Inspect-console (mokomes js) </h1>
+
+        <form class="" action="siusti-klausima.php" method="post" value="">
+          <p></p>
+          <input type="text" name="vardas" placeholder="Jusu vardas" value="Timas">
+          <p></p>
+          <input type="text" name="pavarde" placeholder="Jusu pavarde" value="Logan">
+          <p></p>
+          <input type="text" name="antraste" placeholder="Tema" value="del kainos">
+          <p></p>
+          <textarea          name="klausimas" rows="8" cols="80" placeholder="Jusu klausim" >Kiek kainuoja dviratis</textarea>
+          <p></p>
+          <input type="email" name="pastas" placeholder="Jusu el.pastas" value="as@as.lt" >
+          <p></p>
+
+          <button type="submit" name="button">Siusti klausima</button>
+
+        </form>
+
+
+
+        <!-- ------------------------ -->
+        <script type="text/javascript"  src="libs/bootstrap-4/js/bootstrap.min.js">      </script><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script type="text/javascript"  src="libs/jquery.min.js"> </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <!-- !!! mano js failas pats zemiausias -->
+        <script type="text/javascript"  src="main.js">    </script>
+
+    </body>
+</html>
